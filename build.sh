862b69d22608581a3b7d8b45be7f6be4c86f1cc1
Auston Stewart <auston@shedbuilt.net>
2018-04-15 09:37:25 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-if [ "$SHED_BUILDMODE" != 'toolchain' ]; then
-    echo "This package cannot be built in this build mode: '$SHED_BUILDMODE'"
+if [ "$SHED_BUILD_MODE" != 'toolchain' ]; then
+    echo "This package cannot be built in this build mode: '$SHED_BUILD_MODE'"
     exit 1
 fi
-if [ "$SHED_HOST" != 'toolchain' ]; then
-    echo "This package cannot be built with this host: '$SHED_HOST'"
+if [ "$SHED_BUILD_HOST" != 'toolchain' ]; then
+    echo "This package cannot be built with this host: '$SHED_BUILD_HOST'"
     exit 1
 fi
 mkdir -v build
@@ -17,5 +17,5 @@ cd build
     --disable-libstdcxx-threads     \
     --disable-libstdcxx-pch         \
     --with-gxx-include-dir=/tools/${SHED_TOOLCHAIN_TARGET}/include/c++/7.3.0 && \
-make -j $SHED_NUMJOBS && \
-make DESTDIR="$SHED_FAKEROOT" install || exit 1 
+make -j $SHED_NUM_JOBS && \
+make DESTDIR="$SHED_FAKE_ROOT" install || exit 1 
