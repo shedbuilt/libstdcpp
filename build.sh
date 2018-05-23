0d99c5d852c10c9bbba916b270a16def0897f9dc
Auston Stewart <auston@shedbuilt.net>
2018-05-22 16:59:24 -1000

    Updating for blank package options

--- build.sh
+++ build.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
-if [ "$SHED_BUILD_MODE" != 'toolchain' ]; then
-    echo "This package cannot be built in this build mode: '$SHED_BUILD_MODE'"
+if [ "$SHED_BUILD_HOST" == "$SHED_NATIVE_TARGET" ]; then
+    echo "This package should not be built on the native host"
     exit 1
 fi
-if [ "$SHED_BUILD_HOST" != 'toolchain' ]; then
-    echo "This package cannot be built with this host: '$SHED_BUILD_HOST'"
-    exit 1
-fi
-mkdir -v build
-cd build
+mkdir -v build &&
+cd build &&
 ../libstdc++-v3/configure           \
-    --host=$SHED_TOOLCHAIN_TARGET   \
+    --host=$SHED_BUILD_HOST         \
     --prefix=/tools                 \
     --disable-multilib              \
     --disable-nls                   \
     --disable-libstdcxx-threads     \
     --disable-libstdcxx-pch         \
-    --with-gxx-include-dir=/tools/${SHED_TOOLCHAIN_TARGET}/include/c++/7.3.0 && \
-make -j $SHED_NUM_JOBS && \
-make DESTDIR="$SHED_FAKE_ROOT" install || exit 1 
+    --with-gxx-include-dir=/tools/${SHED_BUILD_HOST}/include/c++/${SHED_PKG_VERSION} &&
+make -j $SHED_NUM_JOBS &&
+make DESTDIR="$SHED_FAKE_ROOT" install
